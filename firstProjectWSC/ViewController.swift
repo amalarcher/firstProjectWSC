6faae20ea7d731f1c721f63a658bffa4aea83132
amalarcher <andre.malarcher@softwaremotifs.com>
2018-09-08 10:41:09 -0400

    Initial Commit

---
+++ firstProjectWSC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  firstProjectWSC
+//
+//  Created by ANDRE MALARCHER on 9/8/18.
+//  Copyright © 2018 Software Motifs LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
